921bdc067e281a8dc1b487e4fd48d3aa8e7e72a3
chris-vanwy <116821693+chris-vanwy@users.noreply.github.com>
2022-11-10 12:26:14 -0700

    Add files via upload

---
+++ VanWy Assignment 4.2Q2/VanWy Assignment 4.2Q2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace VanWy_Assignment_4._2Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstRow, secondRow, size;
+            int[,] firstArray = new int[5, 5];
+            int[,] secondArray = new int[5, 5];
+            int[,] sumOfTheArrays = new int[5, 5];
+
+
+            Console.Write("Input the size of the square matrix (less than 5): ");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            /* Stored values into the array*/
+            Console.Write("\nInput elements in the first matrix :\n");
+            for (firstRow = 0; firstRow < size; firstRow++)
+            {
+                for (secondRow = 0; secondRow < size; secondRow++)
+                {
+                    Console.Write("element - [{0},{1}] : ", firstRow, secondRow);
+                    firstArray[firstRow, secondRow] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.Write("\nInput elements in the second matrix :\n");
+            for (firstRow = 0; firstRow < size; firstRow++)
+            {
+                for (secondRow = 0; secondRow < size; secondRow++)
+                {
+                    Console.Write("element - [{0},{1}] : ", firstRow, secondRow);
+                    secondArray[firstRow, secondRow] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            /* calculate the sum of the matrix */
+            for (firstRow = 0; firstRow < size; firstRow++)
+                for (secondRow = 0; secondRow < size; secondRow++)
+                    sumOfTheArrays[firstRow, secondRow] = firstArray[firstRow, secondRow] + secondArray[firstRow, secondRow];
+            Console.Write("\nThe Sum of two matrixes is : \n");
+            for (firstRow = 0; firstRow < size; firstRow++)
+            {
+                Console.Write("\n");
+                for (secondRow = 0; secondRow < size; secondRow++)
+                    Console.Write("{0}\t", sumOfTheArrays[firstRow, secondRow]);
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
